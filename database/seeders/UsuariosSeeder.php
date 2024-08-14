5f154ed3c37bdedd2d172ae9f774597dbc2cd719
¨JRearte¨ <jrearte198@gmail.com>
2024-08-14 00:32:48 -0300

    14/08/2024

---
+++ database/seeders/UsuariosSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Usuario;
+use Faker\Factory as Faker;
+
+class UsuariosSeeder extends Seeder
+{
+    /**
+     * Ejecuta las pruebas de base de datos : registro de usuarios.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        $categorias = ['Coordinador', 'Maestro', 'Bienestar', 'Invitado'];
+
+        foreach (range(1, 50) as $index) {
+            $legajo = sprintf('%d-%08d/%d', rand(1, 9), $faker->unique()->numberBetween(10000000, 99999999), rand(10, 99));
+
+            Usuario::create([
+                'legajo' => $legajo,
+                'Nombre' => $faker->firstName,
+                'Apellido' => $faker->lastName,
+                'categoria' => $faker->randomElement($categorias),
+                'password' => bcrypt('password'),
+                'habilitado' => $faker->boolean,
+            ]);
+        }
+    }
+}
+
